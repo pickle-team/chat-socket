077e5a1326c4eefe8474bd1d4a5cbcc1d5ada317
SID12g <sid12g@naver.com>
2025-03-11 09:39:21 +0900

    init

---
+++ src/main/java/com/pickle/socket/BasicController.java
@@ -0,0 +1,14 @@
+package com.pickle.socket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    @ResponseBody
+    String hello() {
+        return "hello";
+    }
+}
